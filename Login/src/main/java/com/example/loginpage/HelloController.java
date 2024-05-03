6f5f41f386b85ae73ad6cacee31e120513904b1c
Gustavo Lopes <80708475+GustavoLopes-dev@users.noreply.github.com>
2024-05-03 17:32:16 -0300

    Add files via upload

---
+++ Login/src/main/java/com/example/loginpage/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.loginpage;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Ado ado, quem não me contratar é veado");
+    }
+}
